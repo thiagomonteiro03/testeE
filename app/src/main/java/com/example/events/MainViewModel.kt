075690ca2b4a3a985d2f9cfbcd74aa075ff4451a
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2021-02-01 02:21:24 -0300

    chamada retrofit estruturada com coroutines

---
+++ app/src/main/java/com/example/events/MainViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.events
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainViewModel : ViewModel() {
+    private val service = EventService()
+    private val mEvents = MutableLiveData<List<Event>>()
+
+    val events: LiveData<List<Event>>
+    get() = mEvents
+
+    fun fetchEvents(){
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = service.getEvents()
+
+            if (response.isSuccessful) {
+                withContext(Dispatchers.Main) {
+                    mEvents.value = response.body()
+                }
+            }
+        }
+    }
+
+}
